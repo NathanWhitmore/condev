2cd784553e78c01af0fa3462fb6132c735c1444a
Nathan Whitmore <nwhitmore.work@gmail.com>
2019-03-15 16:21:01 +1300

    update

---
+++ R/Rdata reader.R
@@ -0,0 +1,10 @@
+library(here)
+
+here()
+
+
+load("C:\\Users\\Nathan\\Documents\\R\\R documents\\R codes\\condev\\data\\location.Rdata")
+load("C:\\Users\\Nathan\\Documents\\R\\R documents\\R codes\\condev\\data\\treecover.Rdata")
+load("C:\\Users\\Nathan\\Documents\\R\\R documents\\R codes\\condev\\data\\waihi.Rdata")
+load("C:\\Users\\Nathan\\Documents\\R\\R documents\\R codes\\condev\\data\\sample_points.Rdata")
+
